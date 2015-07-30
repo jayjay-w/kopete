c4fbd5fabcd4af1b42ea0a3c2b6babbb25baff67
Joseph W. Joshua <joejoshw@gmail.com>
2015-07-30 09:05:41 +0300

    Add DatabaseManager class
    The DatabaseManager class contains code to interact with the history
    plugin's database. Functions such as database creation, table creation,
    inserts and searches are handled in this class.
    
    Signed-off-by: Joseph W. Joshua <joejoshw@gmail.com>

---
+++ plugins/history/databasemanager.h
@@ -0,0 +1,81 @@
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+#include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+namespace Kopete {
+class Account;
+class Contact;
+class Message;
+class Protocol;
+}
+
+/**
+ * @brief The DatabaseManager class is used to manage all database connected activities
+ * of the history plugin.
+ */
+class DatabaseManager : public QObject
+{
+	Q_OBJECT
+public:
+	/**
+     * List of the different database systems we use. SQLite is the default,
+	 * and the user is able to select other database systems from the plugin
+	 * preferences.
+	 */
+	enum DatabaseType {
+		SQLITE = 0
+	};
+
+	/**
+	  * Class destructor.
+	  * */
+	~DatabaseManager();
+
+	/**
+     * Initializes the database based on the selected database format.
+	 * This creates the database if it does not exist. For SQLite, this entails
+	 * creating the kopete_history3.db file in the application data folder. For other
+	 * database systems, the database is created on the server, and then all of the
+	 * needed tables are be initialized.
+	 * @param dbType
+	 */
+	void initDatabase(DatabaseType dbType);
+
+	/**
+     * Inserts a chat message into the database. All chat related details such as
+	 * protocol, subject, recepients etc are also logged into the database.
+	 * @param message a Kopete::Message object containing the message to be logged into the
+	 * database.
+	 */
+	void insertMessage(Kopete::Message &message);
+
+	/**
+     * The current instance of the DatabaseManager class.
+	 * @return the value of mInstance
+	 */
+	static DatabaseManager *instance();
+private:
+	/**
+     * DatabaseManager class constructor. If there is no instance of the class in
+	 * existance, a new one will be created.
+	 * @param parent The QObject parent of this class
+	 */
+	explicit DatabaseManager(QObject *parent = 0);
+
+	/**
+     * QSqlDatabase containing the database used to log the chats.
+	 */
+	QSqlDatabase db;
+
+	/**
+     * The current instance of the DatabaseManager class.
+	 */
+	static DatabaseManager *mInstance;
+
+	void bindQueryValue(QSqlQuery query, QString columnName, QString value);
+};
+
+#endif // DATABASEMANAGER_H
